06496b2d8a871cc146445786452658b254e542b5
Sholto <madein.mania@gmail.com>
2021-09-28 10:15:50 +0100

    serving files

--- index.js
+++ index.js
@@ -10,6 +10,19 @@ const server = http.createServer((req, res) => {
     filePath = 'index.html';
   } else filePath = '.' + req.url + '.html';
   console.log(filePath);
+
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      if (err.code == 'ENOENT') {
+        fs.readFile('./404.html', (error, data) => {
+          res.writeHead(200);
+          res.end(data, 'utf-8');
+        });
+      }
+    }
+    res.writeHead(200);
+    res.end(data);
+  });
 });
 
 server.listen(port, () => {
