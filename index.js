63ff44a6845f971630c1dec455de1fba998ef4f7
Sholto <madein.mania@gmail.com>
2021-09-28 09:30:19 +0100

    server listen command

--- index.js
+++ index.js
@@ -5,5 +5,9 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('content-type', 'text/html');
-  res.end();
+  res.end('<p>hi</p>');
+});
+
+server.listen(port, () => {
+  console.log(`server running at port ${port}`);
 });
