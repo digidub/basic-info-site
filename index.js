641c333d43e056cf13dfda2c2fa5359b4f56d969
Sholto <madein.mania@gmail.com>
2021-09-28 10:03:23 +0100

    begin looking at req and req.url

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const http = require('http');
+const fs = require('fs');
 
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('content-type', 'text/html');
-  res.end('<p>hi</p>');
+  console.log(req.url);
+  let filePath = '.' + req.url;
+  console.log(filePath);
 });
 
 server.listen(port, () => {
