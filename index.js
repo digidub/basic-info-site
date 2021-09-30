fecb414e65a523efaf390403e669b6e9ef27d3ac
Sholto <madein.mania@gmail.com>
2021-09-30 10:08:21 +0100

    wrote basic function to serve index page

--- index.js
+++ index.js
@@ -1,34 +1,47 @@
-const http = require('http');
+// const http = require('http');
 const fs = require('fs');
 const express = require('express');
+const app = express();
 
 const port = 3000;
 
-const server = http.createServer((req, res) => {
-  let filePath;
-  if (req.url === '/') {
-    filePath = 'index.html';
-  } else filePath = '.' + req.url + '.html';
-
-  fs.readFile(filePath, (err, data) => {
-    if (err) {
-      if (err.code == 'ENOENT') {
-        fs.readFile('./404.html', (error, data) => {
-          if (error) {
-            console.log(error);
-          }
-          res.writeHead(200);
-          res.end(data);
-          return;
-        });
-      }
-    } else {
-      res.writeHead(200);
-      res.end(data);
-    }
+app.get('/', (req, res) => {
+  fs.readFile('index.html', (err, data) => {
+    res.writeHead(200);
+    res.end(data);
+    return;
   });
 });
 
-server.listen(port, () => {
-  console.log(`server running at port ${port}`);
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}!`);
 });
+
+// const server = http.createServer((req, res) => {
+//   let filePath;
+//   if (req.url === '/') {
+//     filePath = 'index.html';
+//   } else filePath = '.' + req.url + '.html';
+
+//   fs.readFile(filePath, (err, data) => {
+//     if (err) {
+//       if (err.code == 'ENOENT') {
+//         fs.readFile('./404.html', (error, data) => {
+//           if (error) {
+//             console.log(error);
+//           }
+//           res.writeHead(200);
+//           res.end(data);
+//           return;
+//         });
+//       }
+//     } else {
+//       res.writeHead(200);
+//       res.end(data);
+//     }
+//   });
+// });
+
+// server.listen(port, () => {
+//   console.log(`server running at port ${port}`);
+// });
